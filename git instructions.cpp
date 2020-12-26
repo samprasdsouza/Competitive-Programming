28d421d5ba889fd5be66404ccf9ae52ca7913c60
samprasdsouza <samprasdsouza02@gmail.com>
2020-12-26 23:08:47 +0530

    all

---
+++ git instructions.cpp
@@ -0,0 +1,10 @@
+git command
+- terminal - goto the location of the folder/project
+       - git init
+- git status
+- git add
+- git commit -m “…..”
+- git remote add origin “location of remote repo”
+- git push -u origin master
+- git log
+- git --help
