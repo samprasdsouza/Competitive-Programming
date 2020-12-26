4db1cafec5b97bf79874a40fbbd2c9e111d31c10
samprasdsouza <samprasdsouza02@gmail.com>
2020-12-26 23:19:01 +0530

    delete

---
+++ template.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+#define fast ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define endl "\n";
+#pragma GCC optimize "trapv"
+#define sz(v) ((int)(v).size())
+#define rep(i,l,u)   for(int i=l;i<u;i++)
+#define per(i,l,u)  for(int i=l;i>=u;i--)
+const int INF = 1e9;
+const int mx =1e6+100;
+const int mod =1e9+7;
+#define MAX 100005
+using namespace std;
+typedef long long int ll;
+// for policy based DS
+//#include <ext/pb_ds/assoc_container.hpp>
+//#include <ext/pb_ds/tree_policy.hpp>
+//using namespace __gnu_pbds;
+
+//#define ordered_set tree<pair<int,int>, null_type,less<pair<int,int> >, rb_tree_tag,tree_order_statistics_node_update>
+//#define os_find(k) find_by_order(k)
+//#define os_order(k) order_of_key(k)
+template<typename T,typename T1>T amin(T &a,T1 b){if(b<a)a=b;return a;}
+template<typename T,typename T1>T amax(T &a,T1 b){if(b>a)a=b;return a;}
+int main()
+{
+  
+  	int T;
+  	cin>>T;
+  	while(T--)
+  	{
+  		int n;
+  		cin>>n;
+  		while(n--)
+  		{
+  			cout<<"a"<<endl;
+  		}
+  	}
+    return 0;
+}
